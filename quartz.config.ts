e85df9c2b2a0d1e1c56e471db943baabc33446bb
amir1386nasiri <amirmohammadnasiri861117@gmail.com>
2026-01-04 23:55:42 +0330

    Quartz sync: Jan 4, 2026, 11:55 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "fa-ir",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
