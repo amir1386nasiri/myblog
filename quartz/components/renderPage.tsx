f9e83a7394c73184631f3b494f76dcd9d30134af
amir1386nasiri <amirmohammadnasiri861117@gmail.com>
2026-01-05 00:47:34 +0330

    Quartz sync: Jan 5, 2026, 12:47 AM

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -258,7 +258,7 @@ export function renderPage(
   )
 
   const lang = componentData.fileData.frontmatter?.lang ?? cfg.locale?.split("-")[0] ?? "en"
-  const direction = i18n(cfg.locale).direction ?? "rtl"
+
   const doc = (
     <html lang={lang} dir="rtl">
       <Head {...componentData} />
